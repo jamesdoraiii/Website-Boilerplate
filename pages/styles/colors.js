63d4b2e7575b8095e9584ebdf75315568e2cde99
Scott Stewart <scottstewart965@gmail.com>
2019-08-05 11:02:36 -0400

    added colors file

--- pages/styles/colors.js
+++ pages/styles/colors.js
@@ -1,9 +1,15 @@
 const colors = {
-	primary: '#af3009',
-	secondary: '#FF982B',
-	manilla: '#FFEFDE',
-	goldenrod: '#ff982a',
-	darkgray: '#aaaaaa'
-};
+  primary: '#af3009',
+  secondary: '#FF982B',
+  manilla: '#FFEFDE',
+  goldenrod: '#ff982a',
+  darkgray: '#aaaaaa',
+  logoblue: '#39B2E3',
+  logored: '#D95867',
+  logodark: '#40607C',
+  logogold: '#F2A206',
+  backgroundwhite: '#FFFFFF',
+  fontsignup: '#6A6C6F'
+}
 
-module.exports = colors;
+module.exports = colors
